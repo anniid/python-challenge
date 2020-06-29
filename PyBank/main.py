59164be7ca525d18175c4fe0bd1d2bed142a0dec
Annelise Cope <annelisecope@gmail.com>
2020-06-29 09:13:52 -0500

    final edits of pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,22 +1,52 @@
 #import required libraries
 import os
 import csv
-#"read" the csv file to python
+#"read" the csv file to python and setup output
 budget_pth= os.path.join("C:/Users/annel/Desktop/python-challenge/PyBank/Resources/budget_data.csv")
-with open(budget_pth) as csvfile:
-    csv_reader=csv.reader(csvfile, delimiter=",")
-#get a count of months in the budget (there is one row per month)
-    listbud= list((csv_reader))
-    Total_Months=((len(listbud))-1)
-    print(Total_Months)
-    #There are 86 months recorded in the budget data file
-    
-    #Sum the Profits/Losses column to get net total over entire period
+txtOutput=os.path.join("C:/Users/annel/Desktop/python-challenge/PyBank/Analysis/budget_analysis.txt")
+#track variables
+Total_Months=0
+previous_rev=0
+rev_change=0
+change_month=[]
+rev_change_list=[]
+gt_increase=["",0] #so it starts at the beinning month and anything >0
+gt_decrease=["",9999999999999999999] #so it tracks the whichever month and anything < the second number
+net_total=0
+with open(budget_pth) as budget_data:
+    csv_reader=csv.DictReader(budget_data)
+    #loop thru rows to sum things
+    for row in csv_reader:
+        #update variables as they change
+        Total_Months=Total_Months+1
+        net_total=net_total+int(row["Profit/Losses"])
+        rev_change=int(row["Profit/Losses"])-previous_rev
+        previous_rev=int(row["Profit/Losses"])
+        rev_change_list=rev_change_list+[rev_change]
+        change_month=change_month+[row["Date"]]
+            #track greatest increase and decrease
+        if (rev_change>gt_increase[1]):
+            gt_increase[0]=row["Date"]
+            gt_increase[1]=rev_change
+        if (rev_change<gt_decrease[1]):
+            gt_decrease[0]=row["Date"]
+            gt_decrease[1]=rev_change
 
-    #average change in profits/losses over entire period
+#avg change in revenue
+average_rev=sum(rev_change_list)/len(rev_change_list)
 
-    #greatest increase in profits/losses (date and amount)
 
-    #greatest decrease in profits/losses (date and amount)
+#print all the above results and then "write" to a text file
+output=(
+    f"\nFinancial Analysis\n"
+    f"--------------------------------\n"
+    f"Total Months: {Total_Months}\n"
+    f"Net Total: ${net_total}\n"
+    f"Average Change in Revenue: ${average_rev}\n"
+    f"Greatest Increase in Revenue: {gt_increase[0]} (${gt_increase[1]})\n"
+    f"Greatest Decrease in Revenue: {gt_decrease[0]} (${gt_decrease[1]})\n"
+)
+print(output)
 
-    #print all the above results as you go ^^ and then "write" to a text file when done
+with open(txtOutput,"w") as txtFile:
+    txtFile.write(output)
